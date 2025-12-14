7614d754574fb15b3160ddca51d5715cd27036e7
User Pirog <pirogov27@bk.ru>
2025-12-14 22:30:32 +0300

    Коммит 4

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Коммит 3");
+        System.out.println("Коммит 4");
         System.out.println("Это добавлено в ветке main");
     }
 }
